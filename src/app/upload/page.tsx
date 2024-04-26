b9f4723b56ed24e586b70b632190808269b232ce
M-ARTHELL <makoarthell@proton.me>
2024-04-26 20:14:09 -0300

    create upload page

---
+++ src/app/upload/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TopNavbar } from "../components/TopNavbar"
+
+export default function Upload() {
+  return(
+    <main>
+      <span><TopNavbar /></span>
+    </main>
+  )
+}
