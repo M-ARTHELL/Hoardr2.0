88f64598096ed7ddb12df9a54f7ee04b0511d3a0
M-ARTHELL <makoarthell@proton.me>
2024-04-27 22:38:23 -0300

    collect form data

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,27 +1,31 @@
 'use client'
 
-import React from "react";
+import React, { useState } from "react";
 import { TopNavbar } from "../components/TopNavbar";
-import { Uploader } from "../components/Uploader";
 import { TagInput } from "../components/TagInput";
-import { Input, Textarea } from "@nextui-org/react";
-
-
+import { Input, Button, Textarea } from "@nextui-org/react";
 
 export default function Upload() {
+  let [tags, setTags] = useState([]);
 
-  const selectedTags = tags => console.log(tags)
+  function handleSubmitResource(event) {
+    event.preventDefault();
+    const {title, description, image} = event.target.elements;
+    const formData = {title: title.value, description: description.value, image: image.value, tags}
+    console.log(JSON.stringify(formData))
+  }
 
   return(
     <main className="h-screen">
       <span><TopNavbar /></span>
       <div className="m-10 p-10 bg-gray-800 rounded-lg">
-        <form className="flex flex-col justify-evenly">
-          <Input type="text" label="Title" className="mb-10"/>
-          <Uploader />
-          <Textarea type="text" label="Description" className="mt-10"/>
-          <TagInput selectedTags={selectedTags}/>
-          {/* <Button type="submit" className="mt-10 w-20">Submit</Button> */}
+        <form onSubmit={handleSubmitResource} className="flex flex-col justify-evenly">
+          <Input type="text" label="Title" name="title" className="mb-10"/>
+          <input type="file" name="image" />
+          <Textarea label="Description" name="description" className="my-10"/>
+          <TagInput tags={tags} setTags={setTags}/>
+
+          <Button type="submit" className="mt-10 w-20">Submit</Button>
         </form>
       </div>
     </main>
